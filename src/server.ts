0e5dbdcd391812a9535425ec24e613a2c16f3767
SerpianDev-Stack <joaolucasdomingos05@gmail.com>
2025-10-15 18:18:49 -0300

    configurações express

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,12 @@
-const text = "ola";
+import express from "express";
 
+const port = 3000;
+const app = express();
+
+app.get("/movies", (req, res) => {
+    res.send("Listagem de filmes");
+});
+
+app.listen(port, () => {
+    console.log("Servidor em execução");
+});
