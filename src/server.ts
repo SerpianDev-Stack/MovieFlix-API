710a48be4fd339ffb63d98360e7ecaa6087ef997
SerpianDev-Stack <joaolucasdomingos05@gmail.com>
2025-10-17 15:38:58 -0300

    adicionada remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -37,6 +37,24 @@ app.put("/movies/:id", async (req, res) => {
     }
 });
 
+app.delete("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    try {
+        const movie = await prisma.movie.findUnique({ where: { id } });
+
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encotrado" });
+        }
+
+        await prisma.movie.delete({ where: { id } });
+        res.status(200).send();
+    } catch {
+        return res.status(500).send({ message: "Não foi possível remover o filme" });
+    }
+
+});
+
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
 });
